1f3e895e31088621ed6b5a1b7d370a8c43a29d4c
Lorenz Mielke <info@lommix.de>
2023-08-12 11:39:53 +0200

    wip

--- static/main.js
+++ static/main.js
@@ -1,9 +1,14 @@
+// Using js in the intended way, small animations and transitions.
+
+
+// @description Toggle an element
 // @param {string} id
 function toggle(id) {
   const element = document.getElementById(id);
   element.classList.toggle("hidden");
 }
 
+// @description slide in a element
 // @param {string} id
 // @param {number} pixel
 function slide_down(id, pixel) {
